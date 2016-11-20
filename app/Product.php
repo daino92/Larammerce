39d00479ca3f3d1e13d55d88e1e13aaf42a9f7c3
Dionysis Kalepanagos <daino92@gmail.com>
2016-11-20 22:58:03 +0200

    add mapping

--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,62 @@ class Product extends Model
 {
     protected $fillable = ['imagePath', 'title', 'description', 'price'];
     use ElasticquentTrait;
+
+    function getIndexName(){
+        return 'products';
+    }
+
+    protected $indexSettings = [
+        'analysis' => [
+            'char_filter' => [
+                'replace' => [
+                    'type' => 'mapping',
+                    'mappings' => [
+                        '&=> and '
+                    ],
+                ],
+            ],
+            'filter' => [
+                'word_delimiter' => [
+                    'type' => 'word_delimiter',
+                    'split_on_numerics' => false,
+                    'split_on_case_change' => true,
+                    'generate_word_parts' => true,
+                    'generate_number_parts' => true,
+                    'catenate_all' => true,
+                    'preserve_original' => true,
+                    'catenate_numbers' => true,
+                ]
+            ],
+            'analyzer' => [
+                'default' => [
+                    'type' => 'custom',
+                    'char_filter' => [
+                        'html_strip',
+                        'replace',
+                    ],
+                    'tokenizer' => 'whitespace',
+                    'filter' => [
+                        'lowercase',
+                        'word_delimiter',
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    protected $mappingProperties = array(
+        'title' => array(
+            'type' => 'string',
+            'analyzer' => 'standard'
+        ),
+        'description' => array(
+            'type' => 'string',
+            'analyzer' => 'standard'
+        ),
+        'price' => array(
+            'type' => 'integer',
+            'include_in_all' => false
+        )
+    );
 }
