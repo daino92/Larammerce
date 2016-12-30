e581a83894b9135b15d311d6cf847fe1437d2a7b
Dionysis Kalepanagos <daino92@gmail.com>
2016-12-30 23:50:12 +0200

    results.blade.php small update.

--- resources/views/shop/results.blade.php
+++ resources/views/shop/results.blade.php
@@ -6,27 +6,29 @@
 
 @section('content')
     <div id="container">
-        <h3>Your Search for: {{ Request::input('query') }} </h3>
+        <h2 style="text-align: center">You searched for: {{ Request::input('query') }} </h2>
         @if (!$products->count())
-            <p>No Details found. Try to search again!</p>
+            <p>No Details found. Try searching again!</p>
         @else
-            <div class="row">
-                @foreach($products as $product)
-                    <div class="col-sm-6 col-md-3">
-                        <div class="thumbnail">
-                            <img src="{{ $product->imagePath }}" alt="...">
-                            <div class="caption">
-                                <h3>{{ $product->title }}</h3>
-                                <p class="description">{{ $product->description }}</p>
-                                <div class="clearfix">
-                                    <div class="pull-left price">$ {{ $product->price }}</div>
-                                    <a href="{{ route('product.addToCart', ['id' => $product->id]) }}" class="btn btn-success pull-right" role="button">Add to Cart</a>
+            @foreach($products->chunk(4) as $productChunk)
+                <div class="row">
+                    @foreach($productChunk as $product)
+                        <div class="col-sm-6 col-md-3">
+                            <div class="thumbnail">
+                                <img src="{{ $product->imagePath }}" alt="...">
+                                <div class="caption">
+                                    <h3>{{ $product->title }}</h3>
+                                    <p class="description">{{ $product->description }}</p>
+                                    <div class="clearfix">
+                                        <div class="pull-left price">$ {{ $product->price }}</div>
+                                        <a href="{{ route('product.addToCart', ['id' => $product->id]) }}" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
+            @endforeach
         @endif
     </div>
 @endsection
