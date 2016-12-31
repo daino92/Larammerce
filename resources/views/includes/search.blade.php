215eeba92e5d34689ac3794eb26cb696acdf89f3
Dionysis Kalepanagos <daino92@gmail.com>
2016-12-31 11:11:20 +0200

    change name directory.

---
+++ resources/views/includes/search.blade.php
@@ -0,0 +1,17 @@
+<div class="container-fluid search-area" style="background: url('{{ URL::to('pictures/search.jpg') }}');background-repeat:no-repeat;">
+    {!! Form::open(['route' => 'shop.results', 'method' => 'GET', 'role' => 'search']) !!}
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="algn-center">
+                <div id="custom-search-input">
+                    <div class="input-group col-md-12">
+                        <input type="text" name="query" class="form-control input-lg" placeholder="Search ..."/>
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span></button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {!! Form::close() !!}
+</div>
