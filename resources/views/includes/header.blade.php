203cd0bc16b56020cb5955e50cd7f2432b9ad360
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-02 13:40:17 +0200

    update it with vendor.profile.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,6 @@
+<link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
 
-   
- <link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
-   <nav class="navbar navbar-default">
+<nav class="navbar navbar-default">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
@@ -28,13 +27,13 @@
                     @elseif(Auth::check() && Auth::user()->roles->contains(2))
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><i class="fa fa-user fa-2x" aria-hidden="true"></i> {{ Auth::user()->username }} <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="{{ route('user.profile') }}">User Profile</a></li> <!--It is meant to be vendor.profile -->
-                        <li><a href="{{ route('user.logout') }}">Log out</a></li>       <!--It is meant to be vendor.logout -->
+                        <li><a href="{{ route('vendor.profile') }}">User Profile</a></li><!--auth()->user()->username -->
+                        <li><a href="{{ route('user.logout') }}">Log out</a></li>
                     </ul>
                     @elseif(Auth::check() && Auth::user()->roles->contains(3))
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><i class="fa fa-user fa-2x" aria-hidden="true"></i> {{ Auth::user()->username }} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="{{ route('user.profile') }}">User Profile</a></li>
+                            <li><a href="{{ route('user.profile') }}">User Profile</a></li><!--auth()->user()->username -->
                             <li><a href="{{ route('user.logout') }}">Log out</a></li>
                         </ul>
                     @else
