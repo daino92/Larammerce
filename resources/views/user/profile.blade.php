4f0135b26ad8ea3ed36fff105a5fec257dcd9d02
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-07 16:13:14 +0200

    change profile menu.

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -9,18 +9,15 @@
         <div class="container-fluid" style="height:700px;">
             <div class="col-lg-2 left_menu_pos">
                     <div class="row">
-                         <img class="userpic" src="/pictures/userpic.png" alt="" />
-                             <h4 style="text-align:center;">Dionisis Manos</h4>
-                             <hr>
-                             <ul class="profile">
-                                 <a href=""><li>View Profile</li></a>
-                                 <a href=""><li>Profile</li></a>
-                                 <a href=""><li>Account</li></a>
-                                 <a href=""><li>History</li></a>
-                                 <a href=""><li>Privacy</li></a>
-                                 <a href=""><li>Preferences</li></a>
-                                 <a href=""><li>Logout</li></a>
-                             </ul>
+                        <img class="userpic" src="/pictures/userpic.png" alt=""/>
+                        <h4 style="text-align:center;">Dionisis Manos</h4>
+                        <hr>
+                        <ul class="profile">
+                            <a href=""><li>Preferences</li></a>
+                            <a href=""><li>Order History</li></a>
+                            <a href=""><li>Products in Cart</li></a>
+                            <a href=""><li>Logout</li></a>
+                        </ul>
                     </div>
             </div>
             <div class="col-lg-10">
