3a875ca7ca07058414948300e839a10e0bd90642
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-07 23:57:57 +0200

    typos in adduser blade.

--- resources/views/admin/users/adduser.blade.php
+++ resources/views/admin/users/adduser.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Users Management</h1>
+                    <h1 class="page-header">User Management</h1>
                     <ol class="breadcrumb">
                         <li class="active">
                             <span class="fa fa-user"></span> <a href="{{ route('admin.users.allusers') }}">Users</a> / Creation
