ad3ed2ae7e4593dae584021fca7c2b220757f49c
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-08 18:52:02 +0200

    functions in vendor controller.

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class VendorController extends Controller
 {
-    //
-    
-       public function getVendorprofile(){
+    public function VendorProfile(){
         return view('vendor.profile');
+    }
+
+    public function preferences(){
+        return view('vendor.preferences');
+    }
+
+    public function VendorOrders(){
+        return view('vendor.orders');
     }
 }
