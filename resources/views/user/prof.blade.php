e74df81e973f6be988c6e795974a3504d26dc63f
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-08 23:08:01 +0200

    include prof for user.

---
+++ resources/views/user/prof.blade.php
@@ -0,0 +1,13 @@
+<div class="col-lg-2 left_menu_pos">
+    <div class="row">
+        <img class="userpic" src="/pictures/userpic.png" alt=""/>
+        <h4 style="text-align:center;">Dionisis Manos</h4>
+        <hr>
+        <ul class="profile">
+            <li><a href="">Preferences</a></li>
+            <li><a href="{{ route('order.history') }}">Order History</a></li>
+            <li><a href="{{ route('ongoing.orders') }}">Products in Cart</a></li>
+            <li><a href="{{ route('user.logout') }}">Logout</a></li>
+        </ul>
+    </div>
+</div>
