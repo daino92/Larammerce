f052e04ef18235892a6ceeff8d3683d63a9ac37f
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-08 23:09:00 +0200

    order history for user.

--- resources/views/user/orderhistory.blade.php
+++ resources/views/user/orderhistory.blade.php
@@ -1,38 +1,53 @@
-<div class="col-lg-10">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-1">
-            <h1 style="text-align:center;">{{ Auth::user()->username }}'s profile</h1>
-            <hr>
-            <h2><center>Order History</center></h2>
-            @foreach($orders as $order)
-                <div class="panel panel-info">
-                    <div class="panel-body">
-                        @foreach($order->cart->items as $item)
-                            <div class="row">
-                                <div class="col-xs-2">
-                                    <img class="img-responsive" src="{{ $item['item']['imagePath'] }}">
+@extends('master')
+
+@section('styles')
+    <link rel="stylesheet" href="{{URL::to('src/css/user.css')}}">
+@endsection
+
+@section('content')
+    <div id="page-wrapper" class="dashboard">
+        <div class="container-fluid" style="height:700px;">
+            @include('user.prof')
+            <div class="col-lg-10">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-1">
+                        <h1 style="text-align:center;">{{ Auth::user()->username }}'s profile</h1>
+                        <hr>
+                        <h2 style="text-align: center">Order History</h2>
+                        @foreach($orders as $order)
+                            <div class="panel panel-info">
+                                <div class="panel-body">
+                                    @foreach($order->cart->items as $item)
+                                        <div class="row">
+                                            <div class="col-xs-2">
+                                                <img class="img-responsive" src="{{ $item['item']['imagePath'] }}">
+                                            </div>
+                                            <div class="col-xs-4">
+                                                <h4 class="product-name"><strong>{{ $item['item']['title'] }}</strong></h4><h4><small>{{ $item['item']['description'] }}</small></h4>
+                                            </div>
+                                            <div class="col-xs-6">
+                                                <div class="col-xs-6 text-right pull-right">
+                                                    <h4><strong> {{ $item['price'] }} $ <span class="text-muted">- </span>{{ $item['qty'] }} Units</strong></h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                    @endforeach
                                 </div>
-                                <div class="col-xs-4">
-                                    <h4 class="product-name"><strong>{{ $item['item']['title'] }}</strong></h4><h4><small>{{ $item['item']['description'] }}</small></h4>
-                                </div>
-                                <div class="col-xs-6">
-                                    <div class="col-xs-6 text-right pull-right">
-                                        <h4><strong> {{ $item['price'] }} $ <span class="text-muted">- </span>{{ $item['qty'] }} Units</strong></h4>
+                                <div class="panel-footer">
+                                    <div class="row text-center">
+                                        <div class="col-xs-9 pull-right">
+                                            <h4 class="text-right"><strong>Total Price: {{ $order->cart->totalPrice }} $</strong></h4>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <hr>
                         @endforeach
                     </div>
-                    <div class="panel-footer">
-                        <div class="row text-center">
-                            <div class="col-xs-9 pull-right">
-                                <h4 class="text-right"><strong>Total Price: {{ $order->cart->totalPrice }} $</strong></h4>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-            @endforeach
+            </div>
         </div>
     </div>
-</div>
+@endsection
+
+
