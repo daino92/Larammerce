f53a398ea965733d637ec32d4ab82e6c8f0c6de5
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-08 23:08:30 +0200

    dummy profile for user.

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,94 +7,10 @@
 @section('content')
     <div id="page-wrapper" class="dashboard">
         <div class="container-fluid" style="height:700px;">
-            <div class="col-lg-2 left_menu_pos">
-                    <div class="row">
-                        <img class="userpic" src="/pictures/userpic.png" alt=""/>
-                        <h4 style="text-align:center;">Dionisis Manos</h4>
-                        <hr>
-                        <ul class="profile">
-                            <a href=""><li>Preferences</li></a>
-                            <a href=""><li>Order History</li></a>
-                            <a href=""><li>Products in Cart</li></a>
-                            <a href=""><li>Logout</li></a>
-                        </ul>
-                    </div>
-            </div>
+        @include('user.prof')
             <div class="col-lg-10">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-1">
-                        <h1 style="text-align:center;">{{ Auth::user()->username }}'s profile</h1>
-                        <hr>
-                        <h2><center>Order History</center></h2>
-                        @foreach($orders as $order)
-                            <div class="panel panel-info">
-                                <div class="panel-body">
-                                    @foreach($order->cart->items as $item)
-                                        <div class="row">
-                                            <div class="col-xs-2">
-                                                <img class="img-responsive" src="{{ $item['item']['imagePath'] }}">
-                                            </div>
-                                            <div class="col-xs-4">
-                                                <h4 class="product-name"><strong>{{ $item['item']['title'] }}</strong></h4><h4><small>{{ $item['item']['description'] }}</small></h4>
-                                            </div>
-                                            <div class="col-xs-6">
-                                                <div class="col-xs-6 text-right pull-right">
-                                                    <h4><strong> {{ $item['price'] }} $ <span class="text-muted">- </span>{{ $item['qty'] }} Units</strong></h4>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <hr>
-                                    @endforeach
-                                </div>
-                                <div class="panel-footer">
-                                    <div class="row text-center">
-                                        <div class="col-xs-9 pull-right">
-                                            <h4 class="text-right"><strong>Total Price: {{ $order->cart->totalPrice }} $</strong></h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
 
-            @if (Session::has('cart'))
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <h2><center>Ongoing Orders</center></h2>
-                            <div class="panel panel-info">
-                                <div class="panel-body">
-                                    @foreach($products as $product)
-                                        <div class="row">
-                                            <div class="col-xs-2">
-                                                <img class="img-responsive" src="{{ $product['item']['imagePath'] }}">
-                                            </div>
-                                            <div class="col-xs-4">
-                                                <h4 class="product-name"><strong>{{ $product['item']['title'] }}</strong></h4><h4><small>{{ $product['item']['description'] }}</small></h4>
-                                            </div>
-                                            <div class="col-xs-6">
-                                                <div class="col-xs-6 text-right pull-right">
-                                                    <h4><strong> {{ $product['price'] }} $ <span class="text-muted">- </span>{{ $product['qty'] }} Units</strong></h4>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <hr>
-                                    @endforeach
-                                </div>
-                                <div class="panel-footer">
-                                    <div class="row text-center">
-                                        <div class="col-xs-9 pull-right">
-                                            <h4 class="text-right"><strong>Total Price: {{ $totalPrice }} $</strong></h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endif
+            </div>
         </div>
     </div>
 @endsection
