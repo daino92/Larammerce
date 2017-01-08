8e4917f047014e3f0b3224190f229ea42f4d2eaf
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-08 18:40:52 +0200

    prof is include for vendor.

---
+++ resources/views/vendor/prof.blade.php
@@ -0,0 +1,14 @@
+<div class="col-lg-2 left_menu_pos">
+    <div class="row">
+        <img class="userpic" src="/pictures/userpic.png" alt=""/>
+        <h4 style="text-align:center;">Dionisis Manos</h4>
+        <hr>
+        <ul class="profile">
+            <li><a href="{{ route('vendor.preferences') }}">Preferences</a></li>
+            <li><a href="{{ route('vendor.orders') }}">Orders</a></li>
+            <li><a href="{{ route('vendor.allproducts') }}">See my products</a></li>
+            <li><a href="{{ route('vendor.addproducts') }}">Create new products</a></li>
+            <li><a href="{{ route('user.logout') }}">Logout</a></li>
+        </ul>
+    </div>
+</div>
