94b30aa3ade109d7b93dda6c73ffbfd305239e10
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-09 00:17:30 +0200

    fixes in on-going orders.blade.php.

--- resources/views/user/ongoingorders.blade.php
+++ resources/views/user/ongoingorders.blade.php
@@ -9,42 +9,48 @@
         <div class="container-fluid" style="height:700px;">
             @include('user.prof')
             <div class="col-lg-10">
-                @if (Session::has('cart'))
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-1">
-                            <h1 style="text-align:center;">{{ Auth::user()->username }}'s profile</h1>
-                            <hr>
-                            <h2 style="text-align: center">Ongoing Orders</h2>
-                            <div class="panel panel-info">
-                                <div class="panel-body">
-                                    @foreach($products as $product)
-                                        <div class="row">
-                                            <div class="col-xs-2">
-                                                <img class="img-responsive" src="{{ $product['item']['imagePath'] }}">
-                                            </div>
-                                            <div class="col-xs-4">
-                                                <h4 class="product-name"><strong>{{ $product['item']['title'] }}</strong></h4><h4><small>{{ $product['item']['description'] }}</small></h4>
-                                            </div>
-                                            <div class="col-xs-6">
-                                                <div class="col-xs-6 text-right pull-right">
-                                                    <h4><strong> {{ $product['price'] }} $ <span class="text-muted">- </span>{{ $product['qty'] }} Units</strong></h4>
-                                                </div>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-1">
+                        <h1 style="text-align:center;">{{ Auth::user()->username }}'s profile</h1>
+                        <hr>
+                        @if (Session::has('cart'))
+                        <h2 style="text-align: center">Ongoing Orders</h2>
+                        <div class="panel panel-info">
+                            <div class="panel-body">
+                                @foreach($products as $product)
+                                    <div class="row">
+                                        <div class="col-xs-2">
+                                            <img class="img-responsive" src="{{ $product['item']['imagePath'] }}">
+                                        </div>
+                                        <div class="col-xs-4">
+                                            <h4 class="product-name"><strong>{{ $product['item']['title'] }}</strong></h4><h4><small>{{ $product['item']['description'] }}</small></h4>
+                                        </div>
+                                        <div class="col-xs-6">
+                                            <div class="col-xs-6 text-right pull-right">
+                                                <h4><strong> {{ $product['price'] }} $ <span class="text-muted">- </span>{{ $product['qty'] }} Units</strong></h4>
                                             </div>
                                         </div>
-                                        <hr>
-                                    @endforeach
-                                </div>
-                                <div class="panel-footer">
-                                    <div class="row text-center">
-                                        <div class="col-xs-9 pull-right">
-                                            <h4 class="text-right"><strong>Total Price: {{ $totalPrice }} $</strong></h4>
-                                        </div>
+                                    </div>
+                                    <hr>
+                                @endforeach
+                            </div>
+                            <div class="panel-footer">
+                                <div class="row text-center">
+                                    <div class="col-xs-9 pull-right">
+                                        <h4 class="text-right"><strong>Total Price: {{ $totalPrice }} $</strong></h4>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        @else
+                            <div class="row">
+                                <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                                    <h2>No items in Cart!</h2>
+                                </div>
+                            </div>
+                        @endif
                     </div>
-                @endif
+                </div>
             </div>
         </div>
     </div>
