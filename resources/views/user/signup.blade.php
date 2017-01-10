e0ab02b111b80465ab4d6282fb0c8c309d252d1e
Manos <manosgaitanakis93@gmail.com>
2017-01-10 22:33:44 +0200

    fixed height

--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -1,6 +1,7 @@
 @extends('master')
 
 @section('content')
+   <div class="container" style="height:381px;padding-top: 80px;">
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <h1>Sign Up</h1>
@@ -33,4 +34,5 @@
             </form>
         </div>
     </div>
+</div>    
 @endsection
