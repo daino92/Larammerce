c83a2c8390cf92659ac65f9ae2e09125bbef499e
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-13 12:18:29 +0200

    create user and product migrations and added foreign key to user_id in product table.

---
+++ database/migrations/2017_01_11_071410_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('password');
+            $table->rememberToken();
+            $table->boolean('confirmed')->default(0);       //for confirmation mail.
+            $table->string('confirmation_code')->nullable();// -//-
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
