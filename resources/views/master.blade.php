c21dcb291cfc37873b110261d8deca44e0f71fae
Manos <manosgaitanakis93@gmail.com>
2017-01-21 17:10:17 +0200

    linked the new js files and css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,11 +7,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-   <style>
-       *{
-               font-family: abel;
-       }
-    </style>
+    <link rel="stylesheet" href="{{URL::to('src/css/general.css')}}"> 
     @yield('styles')
 </head>
 <body>
@@ -27,6 +23,32 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-show-password/1.0.3/bootstrap-show-password.min.js"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="{{ URL::to('src/js/jquery.catslider.js') }}"></script>
+		<script src="{{ URL::to('src/js/modernizr.custom.63321.js') }}"></script>
+		<script>
+
+                $(document).ready(function(){
+                 $("#divSocial  img").hover(
+                        function () {
+                                        $(this).animate({ top: "-15" });
+                        }, 
+                        function () {
+                                        $(this).animate({ top: "0" });
+                        }
+                    );
+            });
+
+        </script>
+           
+           
+    		<script>
+			$(function() {
+
+				$( '#mi-slider' ).catslider();
+
+			});
+		</script>
+
     @yield('scripts')
     @include('includes.footer')
 </body>
