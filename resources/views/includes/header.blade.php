7941d5ae8b5b346d5064a1024cf21c9c1d8cd589
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-22 01:15:50 +0200

    changes in header.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -15,17 +15,6 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <!--<ul class="nav navbar-nav">
-                <li><a href="#">Shop<span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Vendors</a></li>
-                <li><a href="#">Offers</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                    </ul>
-                </li>
-            </ul>-->
             <ul class="nav navbar-nav navbar-right">
                 <li>
                     <a href="{{ route('product.shoppingCart') }}">
@@ -37,22 +26,22 @@
                     @if (Auth::check() && Auth::user()->roles->contains(1))
                         <a href="{{ route('admin.dashboard') }}" role="button" aria-haspopup="true" aria-expanded="false" ><i class="fa fa-user fa-2x" aria-hidden="true"></i> Administration</a>
                     @elseif(Auth::check() && Auth::user()->roles->contains(2))
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><i class="fa fa-user fa-2x" aria-hidden="true"></i> {{ Auth::user()->username }} <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><img src="{{Auth::user()->avatar}}" class="profPic"> {{ Auth::user()->username }} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="{{ route('vendor.profile') }}">User Profile</a></li><!--auth()->user()->username -->
-                            <li><a href="{{ route('user.logout') }}">Log out</a></li>
+                            <li><a href="{{ route('vendor.profile') }}"><i class="fa fa-user" aria-hidden="true"></i> My Profile</a></li><!--auth()->user()->username -->
+                            <li><a href="{{ route('user.logout') }}"><i class="fa fa-sign-out" aria-hidden="true"></i> Log out</a></li>
                         </ul>
                     @elseif(Auth::check() && Auth::user()->roles->contains(3))
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><i class="fa fa-user fa-2x" aria-hidden="true"></i> {{ Auth::user()->username }} <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><img src="{{Auth::user()->avatar}}" class="profPic"> {{ Auth::user()->username }} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="{{ route('user.profile') }}">User Profile</a></li><!--auth()->user()->username -->
-                            <li><a href="{{ route('user.logout') }}">Log out</a></li>
+                            <li><a href="{{ route('user.profile') }}"><i class="fa fa-user" aria-hidden="true"></i> My Profile</a></li><!--auth()->user()->username -->
+                            <li><a href="{{ route('user.logout') }}"><i class="fa fa-sign-out" aria-hidden="true"></i> Log out</a></li>
                         </ul>
                     @else
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" ><i class="fa fa-user fa-2x" aria-hidden="true"></i> Account <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="{{ route('user.signup') }}">Sign Up</a></li>
-                            <li><a href="{{ route('user.signin') }}">Sign In</a></li>
+                            <li><a href="{{ route('user.signup') }}"><i class="fa fa-user-plus" aria-hidden="true"></i> Sign Up</a></li>
+                            <li><a href="{{ route('user.signin') }}"><i class="fa fa-sign-in" aria-hidden="true"></i> Sign In</a></li>
                         </ul>
                     @endif
                 </li>
