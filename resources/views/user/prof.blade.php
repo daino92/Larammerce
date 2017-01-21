3b7cd85298d9391cb53d6ec671b49e77ad6bdecd
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-22 01:10:36 +0200

    fixed prof.blade in user.

--- resources/views/user/prof.blade.php
+++ resources/views/user/prof.blade.php
@@ -1,10 +1,10 @@
 <div class="col-lg-2 left_menu_pos">
     <div class="row">
-        <img class="userpic" src="/pictures/userpic.png" alt=""/>
-        <h4 style="text-align:center;">Dionisis Manos</h4>
+        <img src="{{Auth::user()->avatar}}" class="userPic">
+        <h4 style="text-align:center;">{{ Auth::user()->username }}</h4>
         <hr>
         <ul class="profile">
-            <li><a href="">Preferences</a></li>
+            <li><a href="{{ route('user.profile') }}">Preferences</a></li>
             <li><a href="{{ route('order.history') }}">Order History</a></li>
             <li><a href="{{ route('ongoing.orders') }}">Products in Cart</a></li>
             <li><a href="{{ route('user.logout') }}">Logout</a></li>
