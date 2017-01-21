76361d46cf76feb27bf34c737fc29fe71d75af4f
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-22 01:34:56 +0200

    add avatar image upload function.

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,26 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\User;
+use App\Order;
+use Auth;
+use Intervention\Image\ImageManagerStatic as Image;
+
 class VendorController extends Controller
 {
-    public function VendorProfile(){
-        return view('vendor.profile');
-    }
-
-    public function preferences(){
+    public function vendorProfile(){
         $user = User::all();
-        return view('vendor.preferences')->withUser($user);;
+        return view('vendor.profile')->withUser($user);
     }
 
-    public function update(){
-        $user = User::find(auth()->user());
-        $user->update($user);
-        return redirect()->route('vendor.preferences');
+    public function update(Request $request, $id){
+        $userUpdate = $request->all();
+        $user = User::find($id);
+
+        $this->validate($request, [
+            //   'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if($file = $request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+            $extension = $file->getClientOriginalName();
+            $username = Auth::user()->username;
+            $destinationPath = public_path('/uploads/avatar/' . $username);
+            $file->move($destinationPath, $extension);
+            $user['avatar'] = '/uploads/avatar/'. $username . '/' . $extension;
+        }
+
+        $user->update($userUpdate);
+        return redirect()->route('vendor.profile');
     }
 
     public function VendorOrders(){
-        return view('vendor.orders');
+        $orders = Order::orderBy('id','ASC')->paginate(5);
+        return view('vendor.orders')->withOrders($orders);
     }
 }
