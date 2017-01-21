d6ce271510e7efa9d5c1f401ebcfbfb2b70dac9d
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-22 01:03:28 +0200

    add profile section to vendor.

--- resources/views/vendor/profile.blade.php
+++ resources/views/vendor/profile.blade.php
@@ -9,7 +9,52 @@
         <div class="container-fluid" style="height:700px;">
             @include('vendor.prof')
             <div class="col-lg-10">
-                profile
+                <div id="page-wrapper" class="dashboard">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h1 class="page-header">User Management</h1>
+                                <ol class="breadcrumb">
+                                    <li class="active">
+                                        <span class="fa fa-user"></span> User / Show
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                        <h1>Update {{Auth::user()->username}}'s information</h1>
+                        {!! Form::model($user,['method' => 'PATCH','route'=>['update.vendor',Auth::user()->id],'files'=>true]) !!}
+                        <fieldset style="margin-right: 600px;">
+                            <div class="form-group">
+                                {!! Form::label('Name', 'Name:') !!}
+                                {!! Form::text('name',Auth::user()->name,['class'=>'form-control']) !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('Surname', 'Surname:') !!}
+                                {!! Form::text('surname',Auth::user()->surname,['class'=>'form-control']) !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('Username', 'Username:') !!}
+                                {!! Form::text('username',Auth::user()->username,['class'=>'form-control']) !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('E-mail', 'E-mail:') !!}
+                                {!! Form::text('email',Auth::user()->email,['class'=>'form-control']) !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('Password', 'Password:') !!}
+                                {!! Form::text('password',Auth::user()->password,['class'=>'form-control']) !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('File', 'Change Profile Picture:') !!}
+                                {!! Form::file('avatar') !!}
+                            </div>
+                            <div class="form-group">
+                                {!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
+                            </div>
+                        </fieldset>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
