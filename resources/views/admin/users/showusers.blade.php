4831214236c8c7c935a55e15b47d91346e6d0bf6
Dionysis Kalepanagos <daino92@gmail.com>
2017-01-22 01:21:36 +0200

    removed pass from showusers.

--- resources/views/admin/users/showusers.blade.php
+++ resources/views/admin/users/showusers.blade.php
@@ -15,46 +15,48 @@
                     </div>
                 </div>
                 <h1>Show {{$user->username}}'s information</h1>
-                <form class="form-horizontal">
-                    <div class="form-group">
-                        <label for="Name" class="col-sm-2 control-label">Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="name" placeholder={{$user->name}} {{$user->surname}} readonly>
+                <fieldset style="margin-right: 600px;">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="Name" class="col-sm-2 control-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name" placeholder={{$user->name}} readonly>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="Username" class="col-sm-2 control-label">Username</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="Username" placeholder={{$user->username}} readonly>
+                        <div class="form-group">
+                            <label for="Surname" class="col-sm-2 control-label">Surname</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="surname" placeholder={{$user->surname}} readonly>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="Password" class="col-sm-2 control-label">Password</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="Password" placeholder={{$user->password}} readonly>
+                        <div class="form-group">
+                            <label for="Username" class="col-sm-2 control-label">Username</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="Username" placeholder={{$user->username}} readonly>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="col-sm-2 control-label">E-mail</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="email" placeholder={{$user->email}} readonly>
+                        <div class="form-group">
+                            <label for="email" class="col-sm-2 control-label">E-mail</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="email" placeholder={{$user->email}} readonly>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="Role" class="col-sm-2 control-label">Role</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="Role" placeholder=
-                            @foreach ($user->roles as $role)
+                        <div class="form-group">
+                            <label for="Role" class="col-sm-2 control-label">Role</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="Role" placeholder=
+                                @foreach ($user->roles as $role)
                                 {{$role->name}}
-                            @endforeach readonly>
+                                @endforeach readonly>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                            <a href="{{ route('admin.users.allusers')}}" class="btn btn-primary">Back</a>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <a href="{{ route('admin.users.allusers')}}" class="btn btn-primary">Back</a>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </fieldset>
             </div>
         </div>
     </div>
