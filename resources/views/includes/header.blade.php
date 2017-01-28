d1220e460971186b1d690d488ce72477062afc83
Manos <manosgaitanakis93@gmail.com>
2017-01-28 16:50:14 +0200

    header bcome a ventor added

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -16,6 +16,11 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
+                       <li>
+                        <a href="#">
+                        <i class="fa fa-user-plus fa-2x" aria-hidden="true" ></i> <span style="color:red">Be a Ventor Now ! </span>
+                    </a>
+                    </li>
                 <li>
                     <a href="{{ route('product.shoppingCart') }}">
                         <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i> Shopping Cart
