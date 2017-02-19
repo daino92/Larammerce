454f2edb50f1d9cd0a2b52943706ec3c0a90d7d3
Dionysis Kalepanagos <daino92@gmail.com>
2017-02-20 01:14:22 +0200

    few changes in products migration.

--- database/migrations/2017_01_11_071426_create_products_table.php
+++ database/migrations/2017_01_11_071426_create_products_table.php
@@ -23,6 +23,9 @@ class CreateProductsTable extends Migration
             $table->string('category',30);
             $table->integer('price');
             $table->integer('stock');
+            $table->integer('rating_count');
+            $table->float('rating_cache',2,1);
+            $table->tinyInteger('published');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
         });
