8c03f13ffed844b6ea5fa8913eaa2fb420ff543f
daino92 <daino92@gmail.com>
2017-05-26 18:18:32 +0300

    new migration for Categories.

---
+++ database/migrations/2017_05_26_065853_create_subcategories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubcategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('image');
+            $table->string('category');
+            $table->string('subcategory');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('subcategories');
+    }
+}
