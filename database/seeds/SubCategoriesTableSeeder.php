1730939ee798ef23f74d48bf7edece400af22e5d
daino92 <daino92@gmail.com>
2017-05-29 01:05:16 +0300

    Fixed CategorySeeder and deleted unnecessary files.

--- database/seeds/SubCategoriesTableSeeder.php
+++ database/seeds/SubCategoriesTableSeeder.php
@@ -11,67 +11,46 @@ class SubCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        
-
-        /*DB::table('SubCategories')->delete();
-        $json = File::get("database/data/subcategories.json");
-        $data = json_decode($json);
-        foreach ($data as $obj) {
-        	SubCategories::create(array(
-            	'id' => $obj->id,
-            	'category' => $obj->category,
-            	'subcategory' => $obj->subcategory
-          	));
-        }*/
-
         SubCategories::create(array(
         	'id' => '1',
-        	'category' => 'Book',
-          'image' => '2.png',
-        	'subcategory' => '{"0":"Action","1":"drama","3":"romance","4":"biography"}'
+        	'category' => 'Books',
+            'image' => '2.png',
+        	'subcategory' => '{"0":"Drama","1":"Horror","3":"Romance","4":"Science fiction", "5":"Fantasy", "6":"Biography", "7":"Action and Adventure"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '2',
-        	'category' => 'electronics',
-          'image' => '1.png',
-        	'subcategory' => '{"0":"smartphones","1":"computers","3":"televisions","4":"gadgets"}'
+        	'category' => 'Technology',
+            'image' => '1.png',
+        	'subcategory' => '{"0":"Mobile Phones","1":"Computers","3":"TVs","4":"Camera & Photo","5":"Audio"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '3',
-        	'category' => 'fashion',
-          'image' => '3.png',
-        	'subcategory' => '{"0":"hoodies","1":"t-shirts","3":"pants","4":"underwear"}'
+        	'category' => 'Fashion',
+            'image' => '3.png',
+        	'subcategory' => '{"0":"T-shirts","1":"Suits","3":"Pants","4":"Shoes","5":"Underwear", "6":"Other Accessories"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '4',
-        	'category' => 'sports',
-          'image' => '4.png',
-        	'subcategory' => '{"0":"snowboards","1":"bikes","3":"tennis","4":"skate"}'
+        	'category' => 'Sports',
+            'image' => '4.png',
+        	'subcategory' => '{"0":"Sportswear","1":"Fitness Equipment","3":"Cycling","4":"Sports","5":"Camping"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '5',
-        	'category' => 'toys',
-          'image' => '5.png',
-        	'subcategory' => '{"0":"babies","1":"kids","3":"adults","4":"newborns"}'
+        	'category' => 'Health & Beauty',
+            'image' => '5.png',
+        	'subcategory' => '{"0":"Cosmetics","1":"Health","3":"Baby Care","4":"Fragrance","5":"Personal Care"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '6',
-        	'category' => 'games',
-          'image' => '6.png',
-        	'subcategory' => '{"0":"action","1":"fps","3":"rpg","4":"mmo"}'
+        	'category' => 'Gaming',
+            'image' => '6.png',
+        	'subcategory' => '{"0":"PS4","1":"Nintendo Switch","3":"X-BOX ONE","4":"PC-Games","5":"Gaming Peripherals","4":"Character Figures"}'
       	));
     }
 }
-/*DB::table('subcategories')->insert([
-    $electronics = array(0=>"smartphones",1=>"computers",2=>"televisions",3=> "gadgets"),
-    $books = array(0=>"action",1=>"drama",2=>"romance", 3=>"biography"),
-    $fashion = array(0=>"hoodies",1=>"t-shirts",2=>"pants",3=> "underwear"),
-    $sports = array(0=>"snowboards",1=>"bikes",2=>"tennis",3=> "skate"),
-    $toys = array(0=>"babies",1=>"kids",2=>"adults"),
-    $games = array(0=>"action",1=>"fps",2=>"rpg",3=> "mmo")
-]);*/
