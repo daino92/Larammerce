4e90dcc0de0fd88e439b9aa63e206e4280f70d87
daino92 <daino92@gmail.com>
2017-05-30 18:07:09 +0300

    minor fix in Category Seeder.

--- database/seeds/SubCategoriesTableSeeder.php
+++ database/seeds/SubCategoriesTableSeeder.php
@@ -14,42 +14,42 @@ class SubCategoriesTableSeeder extends Seeder
         SubCategories::create(array(
         	'id' => '1',
         	'category' => 'Books',
-            'image' => '2.png',
-        	'subcategory' => '{"0":"Drama","1":"Horror","3":"Romance","4":"Science fiction", "5":"Fantasy", "6":"Biography", "7":"Action and Adventure"}'
+          'image' => 'books.jpg',
+        	'subcategory' => '{"0":"Drama","1":"Horror","3":"Romance","4":"Science fiction","5":"Fantasy","6":"Biography", "7":"Action and Adventure","8":"Educational"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '2',
         	'category' => 'Technology',
-            'image' => '1.png',
+          'image' => 'technology.jpg',
         	'subcategory' => '{"0":"Mobile Phones","1":"Computers","3":"TVs","4":"Camera & Photo","5":"Audio"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '3',
         	'category' => 'Fashion',
-            'image' => '3.png',
+          'image' => 'fashion.jpg',
         	'subcategory' => '{"0":"T-shirts","1":"Suits","3":"Pants","4":"Shoes","5":"Underwear", "6":"Other Accessories"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '4',
         	'category' => 'Sports',
-            'image' => '4.png',
+          'image' => 'sports.jpg',
         	'subcategory' => '{"0":"Sportswear","1":"Fitness Equipment","3":"Cycling","4":"Sports","5":"Camping"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '5',
         	'category' => 'Health & Beauty',
-            'image' => '5.png',
+          'image' => 'healthbeauty.jpg',
         	'subcategory' => '{"0":"Cosmetics","1":"Health","3":"Baby Care","4":"Fragrance","5":"Personal Care"}'
       	));
 
       	SubCategories::create(array(
         	'id' => '6',
         	'category' => 'Gaming',
-            'image' => '6.png',
+          'image' => 'gaming.jpg',
         	'subcategory' => '{"0":"PS4","1":"Nintendo Switch","3":"X-BOX ONE","4":"PC-Games","5":"Gaming Peripherals","4":"Character Figures"}'
       	));
     }
