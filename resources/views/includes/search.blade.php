f6029a8a3dd5c7fbb2f59bf226764b6d7bcf6c36
daino92 <daino92@gmail.com>
2017-06-09 17:24:41 +0300

    minor fixes in search blade.

--- resources/views/includes/search.blade.php
+++ resources/views/includes/search.blade.php
@@ -1,13 +1,13 @@
-    {!! Form::open(['route' => 'shop.results', 'method' => 'GET', 'role' => 'search']) !!}
-        {{ csrf_field() }}
-            <div class="algn-center">
-                <div id="custom-search-input">
-                    <div class="input-group col-md-12">
-                        <input type="text" name="query" class="form-control input-lg" placeholder="Search ..."/>
-                        <span class="input-group-btn">
-                            <button type="submit" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span></button>
-                        </span>
-                    </div>
+{!! Form::open(['route' => 'shop.results', 'method' => 'GET', 'role' => 'search']) !!}
+    {{ csrf_field() }}
+        <div class="align-center">
+            <div id="custom-search-input">
+                <div class="input-group col-md-12">
+                    <input type="text" name="query" class="form-control input-lg" placeholder="Search ..."/>
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span></button>
+                    </span>
                 </div>
             </div>
-    {!! Form::close() !!}
+        </div>
+{!! Form::close() !!}
