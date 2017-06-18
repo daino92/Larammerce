dfea7963857bb89353ef5e4bcab6b2db90bf08a9
daino92 <daino92@gmail.com>
2017-06-18 08:55:56 +0300

    corrected the dropdown js functionality.

--- public/src/js/dropdown.js
+++ public/src/js/dropdown.js
@@ -1,13 +1,8 @@
-var $category = $('#category'), $subcategory = $('#subcategory'), $options = $subcategory.find('option');
+$(document).ready(function() {
+    var $category = $('#category'), $subcategory = $('#subcategory'), $options = $subcategory.find('option');
 
-$category.on('change', function() {
-    $subcategory.html($options.filter('[data-options="' + this.value + '"]'));
-} ).trigger('change');
-
-$('#category').change(function(e){
-    $('#subcategory').prop('disabled', !$(this).val());
-});
-
-$(function(){
-    $('#subcategory').prop('disabled', true);
+    $category.on('change', function() {
+        $('#subcategory').prop('disabled', !$(this).val());
+        $subcategory.html($options.filter('[data-options="' + this.value + '"],[value="0"]'));
+    } ).trigger('change');
 });
