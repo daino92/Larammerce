7c41069ca1a4a8b9cb87bf69e899f510182f877b
daino92 <daino92@gmail.com>
2017-09-12 09:48:01 +0300

    fixed order controller in order to work (almost) properly.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,7 +14,7 @@ use Stripe\Charge;
 
 class OrderController extends Controller
 {
-    public function getCheckout(){
+    public function getCheckout(Request $request){
         if(!Session::has('cart')){ // If we don't have a cart, then don't show nothing.
             return view('shop.shopping-cart');
         }
@@ -29,13 +29,20 @@ class OrderController extends Controller
                 $shops[$product['user_id']][0] = $product['price'];
         }
 
-        foreach ($shops as $shop_id => $shop) { // Specific query to bring for every shop the correct ID
+        $shopID = $request->query('sp');
+        $key = DB::select('SELECT stripe_public_key FROM users WHERE id = ?', [$shopID]);
+
+        $shops[$shopID][1] = $key[0]->stripe_public_key; //public key
+
+        $pk_price[$shopID] = $shops[$shopID]; //array of total price and public key
+
+        /*foreach ($shops as $shop_id => $shop) { // Specific query to bring for every shop the correct ID
             $key = DB::select('SELECT stripe_public_key FROM users WHERE id = ?', [$shop_id]);
 
             //print_r($key[0]->stripe_public_key); //prints public key
             $shops[$shop_id][1] = $key[0]->stripe_public_key;
-        }
-        return view('shop.checkout', ['total' => $total, 'shops' => $shops]);
+        }*/
+        return view('shop.checkout', ['total' => $pk_price[$shopID][0], 'shops' => $pk_price]);
     }
 
     public function postCheckout(Request $request){
@@ -46,15 +53,21 @@ class OrderController extends Controller
         $cart = new Cart($emptyCart);
 
         $shop = NULL;
+
+        $remainingShops = array();
         foreach($cart->products as $item) {
             // We get the product id in order to query the database for it. This will perform N queries where N is the number of products.
             $product_id = $item["item"]->id;
             $product = Product::find($product_id);
 
-            if(isset($shop[$product->user_id]))
-                $shop[$product->user_id] += $product->price;
-            else
-                $shop[$product->user_id] = $product->price;
+            if ($product->user_id == $_REQUEST["sp"]) {
+                if (isset($shop[$product->user_id])) {
+                    $shop[$product->user_id] += $product->price;
+                } else {
+                    $shop[$product->user_id] = $product->price;
+                }
+                $remainingShops[] = $product->id;
+            }
         }
 
         try {
@@ -82,10 +95,18 @@ class OrderController extends Controller
 
             Auth::user()->orders()->save($order);
         } catch (\Exception $e){
-            return redirect()->route('checkout')->with('error', $e->getMessage());
+            return redirect()->route('checkout', 'sp=' . $_REQUEST["sp"])->with('error', $e->getMessage());
         }
 
-        Session::forget('cart'); //forget credit card info after purchase
+        for($i = 0; $i < count($remainingShops); $i++){
+            $cart->RemoveProduct($remainingShops[$i]);
+        }
+        if(count($cart->products) > 0)
+            Session::put('cart', $cart);
+        else
+            Session::forget('cart');
+
+        //Session::forget('cart'); //forget credit card info after purchase
         return redirect()->route('indexPage')->with('success', 'Successfully purchased products!');
     }
 }
